f988a9542ff543bcab2bbfa119d1b3c54f5b3155
LibXZR <i@xzr.moe>
2022-04-27 01:32:11 +0800

    kebab: Remove build desc and fingerprint
    Gotta fake it with ppu.
    
    Change-Id: Ie5b3fffee8d7178593be5f3a09ab1b3300308c00

--- arrow_kebab.mk
+++ arrow_kebab.mk
@@ -34,10 +34,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus8T \
-    PRODUCT_NAME=OnePlus8T_EEA \
-    PRIVATE_BUILD_DESC="OnePlus8T-user 11   release-keys"
-
-BUILD_FINGERPRINT := OnePlus/OnePlus8T_EEA/OnePlus8T:11/RP1A.201005.001/2011101425:user/release-keys
+    PRODUCT_NAME=OnePlus8T
 
 # ArrowOS additions
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
