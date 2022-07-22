53bfd1e17387f638bd202079c151867e7f942024
LuK1337 <priv.luk@gmail.com>
2022-07-22 23:15:32 +0800

    lemonades: Set device specific crypto props
    Change-Id: I5f17350fc70af87b9b0b0cfcb4a937f5d4966bae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,9 @@ TARGET_SCREEN_DENSITY := 420
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
 
