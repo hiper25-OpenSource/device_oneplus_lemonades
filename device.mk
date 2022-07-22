fc7d68dba5aefacc0965ca7a2067ec7853e510f4
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-07-22 23:34:24 +0800

    lemonades: Set default hotspot ssid by RRO
    Change-Id: I13fb799a6a0f3c65ed7e8d2751b30f172bd0da5d

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Wifi Overlay
+PRODUCT_PACKAGES += \
+    OnePlus9RWifiOverlay
+
 # Inherit from oneplus sm8250-common
 $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
